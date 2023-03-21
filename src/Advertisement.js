75ad19b26d6e80e36bec8522a16d150a5105ec4f
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-03-21 17:24:26 +0530

    Commit

---
+++ src/Advertisement.js
@@ -0,0 +1,14 @@
+import './Advertisement.js';
+import React from 'react';
+
+function Advertisement() {
+  return (
+    <div id="advertisement">
+        <hr/>
+      Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@ybl .
+        <hr/>
+    </div>
+  );
+}
+
+export default Advertisement;
